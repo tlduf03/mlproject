e5f66b05b83f8812ee1ab8e906d958ffffbb9da0
Kijun Kim <tlduf03@gmail.com>
2023-05-25 13:33:30 -0700

    Data transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
     #inputs 
@@ -48,5 +52,9 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 if __name__ == '__main__':
+    #combine data ingestion and tranformation
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
