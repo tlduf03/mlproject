134202e404d4c6ac7bbb655b8ff392a4ead45ba4
Kijun Kim <tlduf03@gmail.com>
2023-05-28 22:24:16 -0700

    test deployment with debug mode on

--- application.py
+++ application.py
@@ -42,4 +42,4 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
     
 if __name__=="__main__":
-    application.run(host='0.0.0.0') #run on http://127.0.0.1:5000 deafualt
+    application.run(host='0.0.0.0', debug=True) #run on http://127.0.0.1:5000 deafualt
