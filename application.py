88164ddd63eae3c3604eda072d5dfb2398f4e2f5
Kijun Kim <tlduf03@gmail.com>
2023-05-28 23:09:55 -0700

    test deployment

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 #this script is used for AWS deployment
 from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipline
