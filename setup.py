64ec2ca261971830096d9eb6526bebebb263e8f2
Kijun Kim <tlduf03@gmail.com>
2023-05-28 22:10:43 -0700

    deployment editted

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", " ") for req in requirements] #b/c readlines() include '\n' 
+        requirements = [req.replace("\n", "") for req in requirements] #b/c readlines() include '\n' 
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
